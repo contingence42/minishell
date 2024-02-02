c99007680ba7bb96af25642204ccd6f0587cb427
Kylian Daumont <kdaumont@bess-f3r5s8.clusters.42paris.fr>
2024-02-02 10:41:45 +0100

    remove include + printf useless

--- src/main.c
+++ src/main.c
@@ -6,13 +6,11 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 09:20:21 by kdaumont          #+#    #+#             */
-/*   Updated: 2024/01/31 09:48:14 by kdaumont         ###   ########.fr       */
+/*   Updated: 2024/02/02 09:56:18 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-# include <readline/history.h>
-# include <readline/readline.h>
 
 void	show_prompt()
 {
@@ -28,7 +26,6 @@ void	show_prompt()
 		if (ft_strcmp(inpt, "history -c") == 0)
 			rl_clear_history();
 		add_history(inpt);
-		printf("%s", inpt);
 		printf("\n");
 		++i;
 	}
