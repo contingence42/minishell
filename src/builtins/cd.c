90786c181c637e0b785a3b5173d4d75150ec295d
Kylian Daumont <kdaumont@student.42.fr>
2024-02-06 13:51:58 +0100

    change cd int to void

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 14:03:14 by kdaumont          #+#    #+#             */
-/*   Updated: 2024/02/06 13:44:15 by kdaumont         ###   ########.fr       */
+/*   Updated: 2024/02/06 13:50:10 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,17 @@
 
 /* Change the working directory by wanted path, like cd
 @param cmd -> t_command struct pointer
-@return :
-	- 0 : fail execution
-	- 1 : success execution
 */
-int	ft_cd(t_command *cmd)
+void	ft_cd(t_command *cmd)
 {
-	if (ft_strcmp(cmd->cmd_name, "cd") != 0 || !cmd->builtin)
-		return (EXIT_FAILURE);
 	if (ft_stralen(cmd->cmd) == 1)
 	{
 		if (chdir(getenv("HOME")) == -1)
-			return (0);
+			return ;
 	}
 	else if (ft_stralen(cmd->cmd) == 2)
 	{
 		if (chdir(cmd->cmd[1]) == -1)
-			return (0);
+			return ;
 	}
-	else
-		return (0);
-	return (1);
 }
