9ef493559a35c80ae8a3538d12121b9434006bbd
Kylian Daumont <kdaumont@student.42.fr>
2024-02-12 13:21:22 +0100

    echo first impl

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,16 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aattali <aattali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/06 13:15:52 by aattali           #+#    #+#             */
-/*   Updated: 2024/02/06 13:16:50 by aattali          ###   ########.fr       */
+/*   Created: 2024/02/05 09:28:17 by kdaumont          #+#    #+#             */
+/*   Updated: 2024/02/12 13:20:25 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_echo(t_command *commands)
+/* Print the parameters, like 'echo' command
+@param cmd -> t_command struct pointer
+@return :
+	- 0 : fail execution
+	- 1 : success execution
+*/
+int	ft_echo(t_command *cmd)
 {
-	(void)commands;
+	size_t	len;
+	int		i;
+
+	len = ft_stralen(cmd->cmd);
+	if (len > 1 && ft_strncmp(cmd->cmd[1], "-", 1) != 0)
+		i = 1;
+	else
+		i = 2;
+	if (ft_strcmp(cmd->cmd_name, "echo") != 0 || !cmd->builtin)
+		return (EXIT_FAILURE);
+	if (len == 1)
+		return (printf("\n"), 1);
+	if (len == 2 && ft_strncmp(cmd->cmd[1], "-", 1) == 0)
+		return (0);
+	while (cmd->cmd[i])
+	{
+		printf("%s", cmd->cmd[i]);
+		if (i != len - 1)
+			printf(" ");
+		i++;
+	}
+	if (ft_strncmp(cmd->cmd[1], "-", 1) != 0)
+		printf("\n");
+	return (EXIT_SUCCESS);
 }
