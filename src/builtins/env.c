a00913cc98bde1ae0e3c431f99653989f2fc64c3
Andrew Attali <aattali@student.42.fr>
2024-02-15 09:58:48 +0100

    env done

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,13 +6,24 @@
 /*   By: aattali <aattali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 13:15:52 by aattali           #+#    #+#             */
-/*   Updated: 2024/02/14 09:56:14 by aattali          ###   ########.fr       */
+/*   Updated: 2024/02/15 09:57:25 by aattali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_env(t_commands *command)
+/**
+ * @brief print the environment
+ *
+ * @param executor the struct of the exec process
+ */
+void	ft_env(t_executor *executor)
 {
-	(void)command;
+	char	**env;
+	size_t	i;
+
+	env = *(executor->env);
+	i = -1;
+	while (env && env[++i])
+		printf("%s\n", env[i]);
 }
